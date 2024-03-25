f672de662680c29910f188a3be192545c521c5ea
Karol Zadora-Przylecki <karolz@microsoft.com>
2024-03-25 10:52:34 -0700

    Sending cert directly (not saving to a file) works now

--- VsSessionServer/Program.cs
+++ VsSessionServer/Program.cs
@@ -1,43 +1,35 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using VsSessionServer;
-using System.IO;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-var (publicCertFilePath, privateCertFilePath, certPassword) = CertGenerator.GenerateCertFiles();
+using var cert = CertGenerator.GenerateCert();
+var certBytes = cert.Export(X509ContentType.Cert);
+var certEncodedBytes = Convert.ToBase64String(certBytes);
 
-try {
-    Console.WriteLine($"Public cert file path  $env:DEBUG_SESSION_SERVER_CERT_FILE=\"{publicCertFilePath}\"");
-    Console.WriteLine($"Private cert file path: {privateCertFilePath}");
+Console.WriteLine($"Before running the client set  $env:DEBUG_SESSION_SERVER_CERT=\"{certEncodedBytes}\"");
 
-    builder.WebHost.ConfigureKestrel(kestrelOptions =>
-    {
-        kestrelOptions.ListenLocalhost(5213, listenOptions => {
-            listenOptions.UseHttps(privateCertFilePath, certPassword);
-        });
-    });
-
-    var app = builder.Build();
-    var sessionServer = new Server();
-
-    app.MapGet("/", () => "Visual Studio run session server");
-
-    var runSessionApi = app.MapGroup("/run_session");
-
-    runSessionApi.MapPut("/", sessionServer.SessionPut);
-
-    runSessionApi.Map("/notify", sessionServer.SessionNotify);
-
-    app.UseWebSockets();
-
-    app.Run();
-}
-finally
+builder.WebHost.ConfigureKestrel(kestrelOptions =>
 {
-    File.Delete(publicCertFilePath);
-    File.Delete(privateCertFilePath);
-}
+    kestrelOptions.ListenLocalhost(5213, listenOptions => {
+        listenOptions.UseHttps(cert);
+    });
+});
 
+var app = builder.Build();
+var sessionServer = new Server();
 
+app.MapGet("/", () => "Visual Studio run session server");
+
+var runSessionApi = app.MapGroup("/run_session");
+
+runSessionApi.MapPut("/", sessionServer.SessionPut);
+
+runSessionApi.Map("/notify", sessionServer.SessionNotify);
+
+app.UseWebSockets();
+
+app.Run();
